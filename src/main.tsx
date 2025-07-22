37b595a6ebb5af2eeb1e1d74cc936983f5532b35
Giovanni B. Trevisan <54962043+GiovanniTrevisan@users.noreply.github.com>
2025-07-22 00:45:25 -0300

    Adjustments

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <HashRouter>
+  <BrowserRouter>
     <App />
-  </HashRouter>
+  </BrowserRouter>
 )
